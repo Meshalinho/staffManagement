742d0a578c395a7a7b8009942646d1abad64f418
Meshal Alsaleem <sa.meshal.sa@gmail.com>
2021-08-26 19:33:46 +0300

    type: feature
    We added a feature that gives the user the ability to view a specific
    staff member' information

---
+++ staffManagement.py
@@ -0,0 +1,41 @@
+################################################################################################################
+def staffID():
+    inputFile = open("inputCopy.txt", 'r')
+    linesList = inputFile.readlines() # List of the files' lines are stored in linesList
+    contentCopy = []
+    IDList = [] # List to store IDs for future reference
+    linesList.pop(0) # Contents of the header are removed from linesList
+    inputID = str(input("Enter accountID: "))
+    print() #Prints empty line to keep space from the prompt and the header
+    sentinel = True  # To keep track if the inputID is in the IDList or not
+
+    while sentinel == True:
+        try:
+            for i in linesList:
+                contentCopy.append(i.split()) # A list of lists which each element's format is [ID, NAME, HOURS, DAY] is created
+
+            for i in contentCopy:
+                IDList.append(i[0]) # The first element of contentCopy which is the ID is appended to IDList
+
+            if inputID in IDList: # If ID is found, print header and staff info and break out of while loop by appointing sentinel as false
+                index = IDList.index(inputID)
+                print("%10s %15s %10s %10s" % ("Staff ID", "Staff Name", "Hour", "Days"))
+                print("%10s %13s %10s %10s" % (contentCopy[index][0], (contentCopy[index][1] + ' '+ contentCopy[index][2]), contentCopy[index][3] , contentCopy[index][4]))
+                sentinel = False
+
+            if sentinel == True:
+                raise ValueError
+                break
+
+            inputFile.close()
+            enterToContinue()
+
+        except ValueError:
+            print("Error: Invalid account number\n")
+            enterToContinue()
+
+        except IOError:
+            print("Error: file not found\n")
+            enterToContinue() # Automatically returns to main menu
+
+################################################################################################################
